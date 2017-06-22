ede4f5008499cec53b18577c6fcce7cc19f528af
Brad Lundy <crusin67@hotmail.com>
2017-06-21 20:53:44 -0700

    Add second chart

--- Script/taskJS.js
+++ Script/taskJS.js
@@ -5,6 +5,9 @@ var moderateDiff = 0;
 var meduimDiff = 0;
 var difficultDiff = 0;
 var veryDifficultDiff = 0;
+var samTask = 0;
+var terryTask = 0;
+var alanTask = 0;
 
 function CreateTask() {
     event.preventDefault();
@@ -34,6 +37,22 @@ function CreateTask() {
         case "Call your Mom, you're not finishing this task":
             veryDifficultDiff += 1;
             break;
+        default:
+            break;
+    }
+
+    switch (name) {
+        case "Terry":
+            terryTask += 1;
+            break;
+        case "Sam":
+            samTask += 1;
+            break;
+        case "Alan":
+            alanTask += 1;
+            break;
+        default:
+            break;
     }
 
 
@@ -48,6 +67,7 @@ function CreateTask() {
     ulNode.appendChild(liNode);
     parent.appendChild(ulNode);
     drawChart();
+    drawPieChart();
 }
 
 // Callback that creates and populates a data table,
@@ -57,7 +77,29 @@ function drawChart() {
 
     // Create the data table.
     var data = new google.visualization.DataTable();
-    data.addColumn('string', 'Difficulty');
+    data.addColumn('string', 'Who has how many task');
+    data.addColumn('number', 'Number of tasks');
+    data.addRows([
+      ['Terry', terryTask],
+      ['Sam', samTask],
+      ['Alan', alanTask]
+    ]);
+
+    // Set chart options
+    var options = {
+        'title': 'Who has how many tasks',
+        'width': 500,
+        'height': 300
+    };
+
+    // Instantiate and draw our chart, passing in some options.
+    var chart = new google.visualization.ColumnChart(document.getElementById('chart_div'));
+    chart.draw(data, options);
+}
+
+function drawPieChart() {
+    var data = new google.visualization.DataTable();
+    data.addColumn('string', 'Who has how many task');
     data.addColumn('number', 'Number of tasks');
     data.addRows([
       ['Laughable Easy', veryEasyDiff],
@@ -68,18 +110,17 @@ function drawChart() {
       ['Call your Mom, you are not finishing this task', veryDifficultDiff]
     ]);
 
-    // Set chart options
     var options = {
-        'title': 'Number of task grouped by difficulty',
-        'width': 600,
-        'height': 400
+        title: 'Number of different difficulty tasks',
+        width: 400,
+        height: 300
     };
 
-    // Instantiate and draw our chart, passing in some options.
-    var chart = new google.visualization.ColumnChart(document.getElementById('chart_div'));
+    var chart = new google.visualization.PieChart(document.getElementById('piechart_div'));
     chart.draw(data, options);
 }
 
-window.onload = function (){
+window.onload = function () {
     drawChart();
+    drawPieChart();
 }
